27d33814dce474bee38b7043435786bbd574fd04
Jonasdoubleyou <jonaswilms2000@gmail.com>
2020-03-30 17:42:20 +0200

    Push build

---
+++ build/precache-manifest.99a0ea4dd39bc754921f948efd806bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a49be4638ed76e1cbfae642bc3ec48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf19b43a14530f8e1360",
+    "url": "/static/css/main.79e40479.chunk.css"
+  },
+  {
+    "revision": "becaf2ddc072d1247b6e",
+    "url": "/static/js/2.52b57106.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.52b57106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf19b43a14530f8e1360",
+    "url": "/static/js/main.5292359d.chunk.js"
+  },
+  {
+    "revision": "fdb40ee9739192da2bc8",
+    "url": "/static/js/runtime-main.a492136a.js"
+  }
+]);
