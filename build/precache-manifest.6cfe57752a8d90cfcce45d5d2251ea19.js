5cf8cd7f1e9593edd4e42fada43db3e05c8f3936
Jonasdoubleyou <jonaswilms2000@gmail.com>
2020-04-01 15:13:29 +0200

    Push build

---
+++ build/precache-manifest.6cfe57752a8d90cfcce45d5d2251ea19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fef1ffc72a3433ab7f7af4e422683c",
+    "url": "/ux-projects/build/index.html"
+  },
+  {
+    "revision": "308a9cd7fcee6a7a9eee",
+    "url": "/ux-projects/build/static/css/main.0b1f57df.chunk.css"
+  },
+  {
+    "revision": "c8c138baa7db0bf80ef0",
+    "url": "/ux-projects/build/static/js/2.6156b959.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/ux-projects/build/static/js/2.6156b959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308a9cd7fcee6a7a9eee",
+    "url": "/ux-projects/build/static/js/main.6f6817a9.chunk.js"
+  },
+  {
+    "revision": "6acb1c8c8ce5881637e9",
+    "url": "/ux-projects/build/static/js/runtime-main.bc788b38.js"
+  }
+]);
